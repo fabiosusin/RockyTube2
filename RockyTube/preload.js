c22e0153d052392231f913a3fca0396f09e45bd9
fabiossussin <fabio.susin@sigeatende.com.br>
2021-09-27 23:27:04 -0300

    first commit

---
+++ RockyTube/preload.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
+
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+// modify your existing createWindow() function
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadURL(`file://${__dirname}/dist/index.html`)
+}
+// ...
